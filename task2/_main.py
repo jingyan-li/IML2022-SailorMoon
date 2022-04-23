81212fa5abc43f2396f5496702c93ba3b66759cf
JingyanLI <jennea.jy.li@gmail.com>
2022-04-23 23:08:18 +0200

    update subtask2&3

--- task2/_main.py
+++ task2/_main.py
@@ -1,6 +1,6 @@
 from feature_extractor import extract_features
 import subtask1
-
+import subtask2
 import pickle
 import numpy as np
 
@@ -11,17 +11,30 @@ if __name__ == "__main__":
     save_to_csv = True
     feat_test = extract_features(path, "test", save2csv=save_to_csv)
 
+
+    # ########## Make Prediction ############
     # Subtask1
     print("subtask 1")
-    # Use best estimator & params
+    # Get test features
     X1, _ = subtask1.get_features(path=path, split='test')
+    # Use best estimator & params
     model1 = pickle.load(open('./log/subtask1_best.p', "rb"))
     # Predict
     pred1 = model1.predict_proba(X1)
     pred1 = np.array(pred1)[:, :, 1].transpose() # of shape [n_samples, n_classes]
     print("subtask1 done!")
+    del X1
 
-    # TODO Subtask 2
+    # Subtask 2
+    print("subtask 2")
+    # Use best estimator & params
+    X2, _ = subtask2.get_features(path=path, split='test')
+    model2 = pickle.load(open('./log/subtask2_best.p', "rb"))
+    # Predict
+    pred2 = model2.predict_proba(X2)
+    pred2 = np.array(pred2)[:, 1].transpose()  # of shape [n_samples, n_classes]
+    print("subtask2 done!")
+
     # TODO Subtask 3
 
 
--- task2/subtask2.py
+++ task2/subtask2.py
@@ -4,75 +4,22 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 import sklearn.metrics as metrics
 import os
-from sklearn.impute import SimpleImputer
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
+from Labels import SEPSIS
+import pickle
 
-def data():
-    # read files
-    train_data = pd.read_csv('./data/train_feature_extracted.csv').sort_values(by=['pid'])
-    indexNames = train_data.columns.values
-    #print(indexNames)
-    # preprocess data
-    datagroup = pd.DataFrame(train_data)#.interpolate()
+def get_features(path="./data", split="train"):
+    features = pd.read_csv(os.path.join(path, f'{split}_feature_extracted.csv')).sort_values("pid")
+    # Select features
+    X = features.values
+    # Fill nan values
+    X = np.nan_to_num(X)
+    y = None
+    if split == "train":
+        labels = pd.read_csv(os.path.join(path, f'{split}_labels.csv')).sort_values("pid")
+        y = labels[SEPSIS].values.ravel()
+    return X, y
 
-    dataset = datagroup.groupby(by=['pid'])#.fillna(0)
-
-    #imp = SimpleImputer(missing_values=np.nan, strategy = 'mean')
-    #imp.fit(datagroup_data)
-    #imputed=imp.transform(train_data)
-    #store data
-    imputeddata = pd.DataFrame(dataset)
-    print(datagroup.shape)
-    #imputeddata.columns=indexNames
-    imputeddata.to_csv("./data/traindata_imputed.csv")
-
-    test_data = pd.read_csv('./data/test_features_extracted.csv').sort_values(by=['pid'])
-    test_gro = pd.DataFrame(test_data)
-    test_group = test_gro.groupby(by=['pid']).fillna(0)
-    imputedtest = pd.DataFrame(test_group)
-    #imputedtest.columns = indexNames
-    imputedtest.to_csv("./data/testdata_imputed.csv")
-    print("Data imputed")
-
-def train2():
-    train_data =pd.read_csv("./data/traindata_imputed.csv")
-    train_feature = pd.read_csv("./data/train_labels.csv").sort_values(by=["pid"]).fillna(0)
-    feature = train_feature["LABEL_Sepsis"]
-    testimputed = pd.read_csv("./data/testdata_imputed.csv")#interpolate()
-    x = np.array(train_data.values)
-    y = np.array(feature.values)
-    z = np.array(testimputed.values)
-    print(z.shape)
-    print(x.shape)
-    print(y.shape)
-
-    #X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
-    #y = np.array([1, 1, 2, 2])
-    #clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
-    #clf.fit(X, y)
-    #print(X.shape)
-    #print(y.shape)
-    #print(clf.predict([[-0.8, -1]]))
-    #imp = SimpleImputer(missing_values=np.nan, strategy = 'mean')
-    #imp.fit(test_data)
-
-    # GPC
-    #kernel = 1.0*RBF(1.0)
-    #gpc = GaussianProcessClassifier(kernel=kernel, random_state=0)
-    #gpc.fit(train_data,train_feature)
-    #result = gpc.predict(test_group)
-
-    # SVC
-    #clf = make_pipeline(StandardScaler,SVC(gamma='auto'))
-    #clf.fit(x,y)
-    #print(clf.fit(x,y))
-    #result = clf.predict(z)
-    #resultdata = pd.DataFrame(result)
-    #resultdata.to_csv("trainresuult.csv")
+def train2(X, y, log_path="./data/subtask2_cvresults.csv", saveEstimator=True):
     seed = 2022
     rf = RandomForestClassifier(random_state=seed, class_weight="balanced_subsample")
     params = {
@@ -80,19 +27,27 @@ def train2():
         "max_depth": [9, 12, 15],
     }
     clf = GridSearchCV(rf, param_grid=params, scoring='roc_auc', n_jobs=-1, cv=10, verbose=3, refit=True)
-    clf.fit(x, y)
+    clf.fit(X, y)
+    # Get cv results
+    cv_results = pd.DataFrame().from_dict(clf.cv_results_)
+    cv_results.to_csv(log_path)
+    # Make prediction by best_estimator on whole train data
     final_model = clf.best_estimator_
-    pred = final_model.predict_proba(x)
+    pred = final_model.predict_proba(X)
     print(pred.shape)
     pred_ = np.array(pred)[ :, 1].transpose()
     task2 = metrics.roc_auc_score(y, pred_)
     print(f"Overall Train roc_auc: {task2}")
-    return final_model.predict(z)
 
+    if saveEstimator:
+        if not os.path.exists('./log'):
+            os.makedirs('./log')
+        pickle.dump(final_model, open("./log/subtask2_best.p", "wb"))
+    return final_model
 
 if __name__ == "__main__":
     print("Subtask 2")
-    data()
-    z =train2()
-    dbz = pd.DataFrame(z)
-    dbz.to_csv('task2.csv')
+    # Get features
+    X, y = get_features()
+
+    estimator = train2(X, y, saveEstimator=True)
