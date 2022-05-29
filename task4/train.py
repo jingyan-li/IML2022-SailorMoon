e73487f04da544144e0f209a26ba79bb999a5bc5
JingyanLI <jennea.jy.li@gmail.com>
2022-05-30 01:02:31 +0200

    train without hypter-tune

--- task4/train.py
+++ task4/train.py
@@ -37,50 +37,66 @@ if __name__ == "__main__":
     if not os.path.exists(out_root):
         os.makedirs(out_root)
 
-    #################### Load Pretrain Model ##########################
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    torch.cuda.empty_cache()
+    # Storage of embeddings
+    embed_path = out_root / "embed.npz"
+    if os.path.isfile(embed_path):
+        # load existing embedding
+        npzfile = np.load(embed_path)
+        print(f"loaded existing embedding {npzfile.files}")
+        pred_embedding = npzfile["train_embed"]
+        pred_y_target = npzfile["train_target"]
+        test_embedding = npzfile["test_embed"]
+        test_y_idx = npzfile["test_y_idx"]
+    else:
+        print("Extracting embedding from pretrain model...")
+        #################### Load Pretrain Model ##########################
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        torch.cuda.empty_cache()
 
-    # Initialize pretrain model
-    pretrain_model = PretrainEmbeddingNet(**config['pretrain_model_cfg'])
+        # Initialize pretrain model
+        pretrain_model = PretrainEmbeddingNet(**config['pretrain_model_cfg'])
 
-    # To gpu
-    pretrain_model = pretrain_model.to(device)
+        # To gpu
+        pretrain_model = pretrain_model.to(device)
 
-    # Load Checkpoints
-    checkpoint_path = Path(train_config['pretrain_checkpoint']) / 'checkpoints' / 'best'
-    pretrain_model.load_state_dict(torch.load(checkpoint_path)['model'])
-    pretrain_model.eval()
-    print("Pretrain model loaded!")
+        # Load Checkpoints
+        checkpoint_path = Path(train_config['pretrain_checkpoint']) / 'checkpoints' / 'best'
+        pretrain_model.load_state_dict(torch.load(checkpoint_path)['model'])
+        pretrain_model.eval()
+        print("Pretrain model loaded!")
 
-    #################### Extract embeddings for train / test ##########################
-    # Set up datasets
-    data = MolecularData(load_sets=['train', 'test'], train_val_split=False, **config["data_cfg"])
-    # Run model to get embedding for train
-    pred_embedding = []
-    pred_y_target = []
-    with torch.no_grad():
-        for step, sample_dict in enumerate(data.dataloaders["train"]):
-            feature, target, indices = sample_dict["feature"], sample_dict["label"], sample_dict["index"]
-            embedding = pretrain_model.get_embedding(feature.to(device))
-            pred_embedding.append(embedding)
-            pred_y_target.append(target)
-    pred_embedding = torch.cat(pred_embedding, 0).cpu().numpy()
-    pred_y_target = torch.cat(pred_y_target, 0).numpy()
-    print("Feature extracted: train ready!")
+        #################### Extract embeddings for train / test ##########################
+        # Set up datasets
+        data = MolecularData(load_sets=['train', 'test'], train_val_split=False, **config["data_cfg"])
+        # Run model to get embedding for train
+        pred_embedding = []
+        pred_y_target = []
+        with torch.no_grad():
+            for step, sample_dict in enumerate(data.dataloaders["train"]):
+                feature, target, indices = sample_dict["feature"], sample_dict["label"], sample_dict["index"]
+                embedding = pretrain_model.get_embedding(feature.to(device))
+                pred_embedding.append(embedding)
+                pred_y_target.append(target)
+        pred_embedding = torch.cat(pred_embedding, 0).cpu().numpy()
+        pred_y_target = torch.cat(pred_y_target, 0).numpy()
+        print("Feature extracted: train ready!")
 
-    # Run model to get embedding for test
-    test_embedding = []
-    test_y_idx = []
-    with torch.no_grad():
-        for step, sample_dict in enumerate(data.dataloaders["test"]):
-            feature, indices = sample_dict["feature"], sample_dict["index"]
-            embedding = pretrain_model.get_embedding(feature.to(device))
-            test_embedding.append(embedding)
-            test_y_idx.append(indices)
-    test_embedding = torch.cat(test_embedding, 0).cpu().numpy()
-    test_y_idx = torch.cat(test_y_idx, 0).numpy()
-    print("Feature extracted: test ready!")
+        # Run model to get embedding for test
+        test_embedding = []
+        test_y_idx = []
+        with torch.no_grad():
+            for step, sample_dict in enumerate(data.dataloaders["test"]):
+                feature, indices = sample_dict["feature"], sample_dict["index"]
+                embedding = pretrain_model.get_embedding(feature.to(device))
+                test_embedding.append(embedding)
+                test_y_idx.append(indices)
+        test_embedding = torch.cat(test_embedding, 0).cpu().numpy()
+        test_y_idx = torch.cat(test_y_idx, 0).numpy()
+        print("Feature extracted: test ready!")
+
+        # Save the embedding
+        np.savez(open(embed_path,"wb"), train_embed=pred_embedding, train_target=pred_y_target, test_embed=test_embedding, test_y_idx=test_y_idx)
+        print("Embedding saved!")
 
     #################### Training ##########################
     # Fit the model
